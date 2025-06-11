b0d0414c93d7bfa5542564c1fb0175eb4b1a526b
kumarsai234 <63974805+kumarsai234@users.noreply.github.com>
2025-06-11 19:47:03 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="bg-dark text-white text-center p-3 mt-5">
+  &copy; <?= date("Y") ?> IBEON Clone. All rights reserved.
+</footer>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Ibeon Clone</title>
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">IBEON</a>
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav ms-auto">
+      <li><a class="nav-link" href="index.php">Home</a></li>
+      <li><a class="nav-link" href="about.php">About</a></li>
+      <li><a class="nav-link" href="services.php">Services</a></li>
+      <li><a class="nav-link" href="contact.php">Contact</a></li>
+    </ul>
+  </div>
+</nav>
